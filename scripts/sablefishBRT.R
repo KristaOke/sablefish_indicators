8167ca35f372fd98e8adfd6953d135ccc2439edc
Krista.Oke <krista.oke@nmfs.local>
2023-02-27 16:56:49 -0900

    predictions and data manipulations

--- scripts/sablefishBRT.R
+++ scripts/sablefishBRT.R
@@ -39,8 +39,22 @@ scaled_dat <- dat %>% #group_by(Year) %>%
 
 brtdat <- scaled_dat
 brtdat <- brtdat[is.na(brtdat$recruit_scaled)==FALSE,] #remove NAs from residuals
-rownames(brtdat) <- brtdat[,1]
-brtdat <- brtdat[,-1]
+
+
+#NEED TO SUBSET out a training dataset
+datlen <- length(brtdat$recruit_scaled)
+train <- brtdat[sample(nrow(brtdat),(round(datlen*0.8))),]
+train <- train[order(row.names(train)),]
+
+testing <- anti_join(brtdat, train)
+
+#save this training set?
+
+rownames(train) <- train[,1]
+train <- train[,-1]
+
+rownames(testing) <- testing[,1]
+testing <- testing[,-1]
 
 
 #Combine matrices
@@ -52,48 +66,17 @@ brtdat <- brtdat[,-1]
 # Fit BRT Model ======================================================
 # if(fit==TRUE) {
 
-fit.covars <- names(brtdat[,11:18]) 
+
+train <- train[,10:18]
+res <- names(train[1])
+
+testing <- testing[,10:18]
+
+fit.covars <- names(train[,c(2:4, 7:9)]) #NO mean age or evenness see below
 #fit.covars <- fit.covars[1:4] #to get things running lets subset out just a few
 
 form.covars <- paste(fit.covars, collapse=" + ")
-form <- paste(names(brtdat[10]), "~",form.covars)
-
-brtdat <- brtdat[,10:18]
-res <- names(brtdat[1])
-
-#NEED TO SUBSET out a training dataset
-datlen <- length(brtdat$recruit_scaled)
-train <- brtdat[sample(nrow(brtdat),(round(datlen*0.8))),]
-train <- train[order(row.names(train)),]
-
-#save this training set?
-
-#NOW make dataframe of test data that's not in the training set!
-
-#playing with parameters------
-
-#see paper section on simplifying the predictor set esp imp if some predictors are repetitive and n is small
-
-temp.fit <- gbm(formula(form), data=brtdat, distribution='gaussian', n.trees=1e4, interaction.depth=1, n.minobsinnode=1)
-
-temp.fit.2 <- gbm.step(data=brtdat, gbm.y=res, gbm.x=fit.covars, family='gaussian', n.minobsinnode=1)
-gbm.plot(temp.fit.2)
-gbm.plot.fits(temp.fit.2)
-
-temp.fit.3 <- gbm(formula(form), data=brtdat, distribution='gaussian', n.minobsinnode=1, n.trees=1e5, shrinkage=0.001, interaction.depth=1)#, interaction.depth=1, cv.folds=1, n.minobsinnode=1)
-gbm.plot(temp.fit.3) #not working?
-
-temp.fit.4 <- gbm.step(data=brtdat, gbm.y=res, gbm.x=fit.covars, family='gaussian', 
-                                       tree.complexity = 1, #b/c very small sample
-                                       learning.rate = 0.005, #slower b/c tc is low and want enough trees
-                                       bag.fraction = 0.7) #paper recommends not fewer than 1000 trees
-gbm.plot(temp.fit.4)
-gbm.plot.fits(temp.fit.4)
-summary(temp.fit.4)
-
-temp.fit.5 <- gbm(formula(form), data=brtdat, distribution='gaussian', bag.fraction=0.7, n.trees=1e5, shrinkage=0.001, interaction.depth=1)#, interaction.depth=1, cv.folds=1, n.minobsinnode=1)
-gbm.plot(temp.fit.5)
-summary(temp.fit.5)
+form <- paste(names(train[1]), "~",form.covars)
 
 
 #getting worried about mean age, is it important because it is - by definition - autocorrelated (w 2 yr lag)
@@ -101,18 +84,38 @@ summary(temp.fit.5)
 
 #try without mean age or evenness
 
-brtdatsub <- brtdat[,10:18]
 
-fit.covars <- names(brtdatsub[,c(2:4, 7:9)]) 
-#fit.covars <- fit.covars[1:4] #to get things running lets subset out just a few
 
-form.covars <- paste(fit.covars, collapse=" + ")
-form <- paste(names(brtdat[10]), "~",form.covars)
+
+#playing with parameters------
+
+#see paper section on simplifying the predictor set esp imp if some predictors are repetitive and n is small
+
+temp.fit <- gbm(formula(form), data=train, distribution='gaussian', n.trees=1e4, interaction.depth=1, n.minobsinnode=1)
+
+temp.fit.2 <- gbm.step(data=train, gbm.y=res, gbm.x=fit.covars, family='gaussian', n.minobsinnode=1)
+gbm.plot(temp.fit.2)
+gbm.plot.fits(temp.fit.2)
+
+temp.fit.3 <- gbm(formula(form), data=train, distribution='gaussian', n.minobsinnode=1, n.trees=1e5, shrinkage=0.001, interaction.depth=1)#, interaction.depth=1, cv.folds=1, n.minobsinnode=1)
+gbm.plot(temp.fit.3) #not working?
+
+temp.fit.4 <- gbm.step(data=train, gbm.y=res, gbm.x=fit.covars, family='gaussian', 
+                                       tree.complexity = 1, #b/c very small sample
+                                       learning.rate = 0.005, #slower b/c tc is low and want enough trees
+                                       bag.fraction = 0.7) #paper recommends not fewer than 1000 trees
+gbm.plot(temp.fit.4)
+gbm.plot.fits(temp.fit.4)
+summary(temp.fit.4)
+
+temp.fit.5 <- gbm(formula(form), data=train, distribution='gaussian', bag.fraction=0.7, n.trees=1e5, shrinkage=0.001, interaction.depth=1)#, interaction.depth=1, cv.folds=1, n.minobsinnode=1)
+gbm.plot(temp.fit.5)
+summary(temp.fit.5)
 
 
 res <- names(brtdat[1])
 
-temp.fit.6 <- gbm.step(data=brtdat, gbm.y=res, gbm.x=fit.covars, family='gaussian', 
+temp.fit.6 <- gbm.step(data=train, gbm.y=res, gbm.x=fit.covars, family='gaussian', 
                        tree.complexity = 1, #b/c very small sample
                        learning.rate = 0.005, #slower b/c tc is low and want enough trees
                        bag.fraction = 0.7) #paper recommends not fewer than 1000 trees
@@ -125,15 +128,43 @@ summary(temp.fit.6)
 
 #see goood tutorial on https://afit-r.github.io/tree_based_methods
 
-res <- names(brtdat[1])
+res <- names(train[1])
 
 real.fit.1 <- gbm.step(data=train, gbm.y=res, gbm.x=fit.covars, family='gaussian', 
+#real.fit.1 <- gbm.step(data=train, gbm.y=10, gbm.x=11:18, family='gaussian', 
                        tree.complexity = 1, #b/c very small sample
                        learning.rate = 0.005, #slower b/c tc is low and want enough trees, paper recommends not fewer than 1000 trees
-                       bag.fraction = 0.81) #won't run with bag fraction lower than 0.8 for training dataset of 34 years
+                       bag.fraction = 0.8,
+                       n.minobsinnode=1) #won't run with bag fraction lower than 0.8 for training dataset of 34 years
 gbm.plot(real.fit.1)
 gbm.plot.fits(real.fit.1)
 gbm.perspec(real.fit.1, x=1, y=2) #not working
 summary(real.fit.1)
+gbm.simplify(real.fit.1, n.drops = 3) #error that nTrain * bag.fraction <= n.minobsinnode` but it is not!!
+gbm.interactions(real.fit.1)
+perf_n <- gbm.perf(real.fit.1)[1] #optimal # trees 264 but that's pretty low
 
-best.real.1 <- gbm.perf(real.fit.1)
+preds <- predict.gbm(real.fit.1, testing, n.trees = perf_n, type="response")
+
+dev <- calc.deviance(obs=testing$recruit_scaled, pred=testing$predictions, family="gaussian", 
+              calc.mean=TRUE)
+
+
+testing$predictions <- preds
+ggplot(testing, aes(predictions, recruit_scaled)) + geom_point() + geom_abline()
+
+sum_squared_errors <- sum((preds-testing$recruit_scaled)^2, na.rm=TRUE)
+
+#can I find a covar that's messing everything up? No
+real.fit.2 <- gbm.step(data=train, gbm.y=10, gbm.x=c(11:13,16:18), family='gaussian', 
+                       tree.complexity = 1, #b/c very small sample
+                       learning.rate = 0.005, #slower b/c tc is low and want enough trees, paper recommends not fewer than 1000 trees
+                       bag.fraction = 0.8,
+                       n.minobsinnode=1,
+                       interaction.depth=3) #won't run with bag fraction lower than 0.8 for training dataset of 34 years
+gbm.plot(real.fit.2)
+gbm.plot.fits(real.fit.2)
+gbm.perspec(real.fit.2, x=1, y=2) #not working
+summary(real.fit.2)
+gbm.simplify(real.fit.2, n.drops = 3) #error that nTrain * bag.fraction <= n.minobsinnode` but it is not!!
+gbm.interactions(real.fit.2) #interactions continue to not work
