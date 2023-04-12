8706f766752a8891798fd0476d0bd68e430b7ee8
KristaOke <kristaoke@gmail.com>
2023-04-12 14:07:17 -0800

    reran and replotted with smaller covariate subset

--- scripts/fit_BAS_sablefish.R
+++ scripts/fit_BAS_sablefish.R
@@ -143,21 +143,21 @@ pred.bas <- predict(bas.lm, estimator="BMA")
 dat.temp.na.omit <- na.omit(train1_bas_dat[-which(names(train1_bas_dat) %in% c("Smr_temp_250m_GOA_scaled"))])
 
 plot(x=dat.temp.na.omit$ln_rec, y=pred.bas$Ybma,
-     xlab="Observed ln(Rec)", ylab="Predicted ln(Rec)", pch=21, bg=rgb(1,0,0,alpha=0.5),
+     xlab="Observed ln(Recruitment)", ylab="Predicted ln(Recruitment)", pch=21, bg=rgb(1,0,0,alpha=0.5),
      main=paste("Sablefish"))
 # plot(x=pred.bas$fit, y=pred.bas$Ybma) 
 abline(a=0, b=1, col=rgb(0,0,1,alpha=0.5), lwd=3)
 
 # Timeseries
 plot(x=dat.temp.na.omit$Year, y=dat.temp.na.omit$ln_rec,
-     xlab="Year", ylab="ln(Rec)", type='l', col=rgb(1,0,0,alpha=0.5),
+     xlab="Year", ylab="ln(Recruitment)", type='l', col=rgb(1,0,0,alpha=0.5),
      main=paste("Sablefish"))
 grid(lty=3, col='dark gray')
 points(x=dat.temp.na.omit$Year, y=dat.temp.na.omit$ln_rec,
        pch=21, bg=rgb(1,0,0,alpha=0.5))
 lines(x=dat.temp.na.omit$Year, y=pred.bas$Ybma, lwd=3, col=rgb(0,0,1, alpha=0.5))
 
-legend('top', legend=c("Observed","Predicted"), lty=1, col=c(rgb(1,0,0,alpha=0.5),
+legend('topleft', legend=c("Observed","Predicted"), lty=1, col=c(rgb(1,0,0,alpha=0.5),
                                                              rgb(0,0,1, alpha=0.5)), bg="white")
 
 dev.off()
@@ -197,11 +197,28 @@ cond.sd <- coef(bas.lm)$conditionalsd
 names(coef(bas.lm))
 
 
+
 #Plot it out....
 par(mfrow=c(1,2), mar=c(4,1,2,1), oma=c(0,10,1,1))
 
 plot.df <- data.frame(bas.names, inc.probs, post.mean, post.sd, low.95, up.95)
 # plot.list <- melt(plot.df)
+
+#let's make the labels pretty
+
+plot.df$bas.names[which(plot.df$bas.names=="Spr_ST_SEBS_scaled")] <- "Spring SST SEBS"
+plot.df$bas.names[which(plot.df$bas.names=="Smr_temp_250m_GOA_scaled")] <- "Summer 250m temperature GOA"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_biom_GOA_scaled")] <- "Spring chlorophyll A biomass GOA"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_biom_SEBS_scaled")] <- "Spring chlorophyll A biomass SEBS"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_peak_GOA_scaled")] <- "Spring chlorophyll A peak GOA"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_peak_SEBS_scaled")] <- "Spring chlorophyll A peak SEBS"
+plot.df$bas.names[which(plot.df$bas.names=="ann_Copepod_size_EGOA_scaled")] <- "Annual copepod community size EGOA"
+plot.df$bas.names[which(plot.df$bas.names=="YOY_grwth_Middleton_scaled")] <- "YOY growth Middleton Is. seabirds"
+plot.df$bas.names[which(plot.df$bas.names=="Smr_CPUE_juv_ADFG_ln_scaled")] <- "Summer juvenile CPUE ADFG survey"
+plot.df$bas.names[which(plot.df$bas.names=="smr_adult_cond_scaled")] <- "Summer adult condition"
+
+
+
 
 # g <- ggplot(filter(plot.df, bas.names!='Intercept'),
 #             aes(x=bas.names, post.mean, fill=bas.names)) +
@@ -293,24 +310,20 @@ ggsave(file=file.path(dir.figs,"BAS_noRainbow.png"), plot=g3.b, height=5, width=
 
 #REPEAT without short time series================================================================
 
-#some covars are not important and also short, let's try removing two of them
-#Smr_temp_250m_GOA_scaled starts 2005
-#Smr_condition_fem_age4_GOA_scaled ends 2017
-
+#use only middleton, condition, sebs sst, adfg
 
 #Determine Covariates
 
-covars <- names(train1_bas_dat)[-which(names(train1_bas_dat) %in% c("Year", "ln_rec",
-                                                                    "Smr_temp_250m_GOA_scaled",
-                                                                    "Smr_condition_fem_age4_GOA_scaled"))]
+covars <- names(train1_bas_dat)[which(names(train1_bas_dat) %in% c("Year", "ln_rec",
+                                                                   "Spr_ST_SEBS_scaled",
+                                                                   "smr_adult_cond_scaled",
+                                                                   "YOY_grwth_Middleton_scaled",
+                                                                   "Smr_CPUE_juv_ADFG_ln_scaled"))]
 
 n.cov <- length(covars)
 
 dat.temp <- train1_bas_dat[-which(names(train1_bas_dat) %in% c("Year"))]
-
-#more parameters than data, let's try dropping the shortest time series
-#Smr_temp_250m_GOA_scaled
-dat.temp <- dat.temp[-which(names(dat.temp) %in% c("Smr_temp_250m_GOA_scaled", "Smr_condition_fem_age4_GOA_scaled"))]
+dat.temp <- dat.temp[which(names(dat.temp) %in% covars)]
 
 # Bayesian Model Selection
 bas.lm <-  bas.lm(ln_rec ~ ., data=dat.temp,
@@ -331,17 +344,19 @@ par(oma=c(1,1,1,1), mar=c(4,4,1,1), mfrow=c(1,2))
 pred.bas <- predict(bas.lm, estimator="BMA")
 
 # Omit NAs
-dat.temp.na.omit <- na.omit(train1_bas_dat[-which(names(train1_bas_dat) %in% c("Smr_temp_250m_GOA_scaled", "Smr_condition_fem_age4_GOA_scaled"))])
+#dat.temp.na.omit <- na.omit(train1_bas_dat[-which(names(train1_bas_dat) %in% c("Smr_temp_250m_GOA_scaled", "Smr_condition_fem_age4_GOA_scaled"))])
+dat.temp.na.omit <- na.omit(train1_bas_dat[which(names(train1_bas_dat) %in% c("Year", "ln_rec", covars))])
+
 
 plot(x=dat.temp.na.omit$ln_rec, y=pred.bas$Ybma,
-     xlab="Observed ln(Rec)", ylab="Predicted ln(Rec)", pch=21, bg=rgb(1,0,0,alpha=0.5),
+     xlab="Observed ln(Recruitment)", ylab="Predicted ln(Recruitment)", pch=21, bg=rgb(1,0,0,alpha=0.5),
      main=paste("Sablefish"))
 # plot(x=pred.bas$fit, y=pred.bas$Ybma) 
 abline(a=0, b=1, col=rgb(0,0,1,alpha=0.5), lwd=3)
 
 # Timeseries
 plot(x=dat.temp.na.omit$Year, y=dat.temp.na.omit$ln_rec,
-     xlab="Year", ylab="ln(Rec)", type='l', col=rgb(1,0,0,alpha=0.5),
+     xlab="Year", ylab="ln(Recruitment)", type='l', col=rgb(1,0,0,alpha=0.5),
      main=paste("Sablefish"))
 grid(lty=3, col='dark gray')
 points(x=dat.temp.na.omit$Year, y=dat.temp.na.omit$ln_rec,
@@ -394,6 +409,20 @@ par(mfrow=c(1,2), mar=c(4,1,2,1), oma=c(0,10,1,1))
 plot.df <- data.frame(bas.names, inc.probs, post.mean, post.sd, low.95, up.95)
 # plot.list <- melt(plot.df)
 
+plot.df$bas.names[which(plot.df$bas.names=="Spr_ST_SEBS_scaled")] <- "Spring SST SEBS"
+plot.df$bas.names[which(plot.df$bas.names=="Smr_temp_250m_GOA_scaled")] <- "Summer 250m temperature GOA"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_biom_GOA_scaled")] <- "Spring chlorophyll A biomass GOA"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_biom_SEBS_scaled")] <- "Spring chlorophyll A biomass SEBS"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_peak_GOA_scaled")] <- "Spring chlorophyll A peak GOA"
+plot.df$bas.names[which(plot.df$bas.names=="Spr_chlA_peak_SEBS_scaled")] <- "Spring chlorophyll A peak SEBS"
+plot.df$bas.names[which(plot.df$bas.names=="ann_Copepod_size_EGOA_scaled")] <- "Annual copepod community size EGOA"
+plot.df$bas.names[which(plot.df$bas.names=="YOY_grwth_Middleton_scaled")] <- "YOY growth Middleton Is. seabirds"
+plot.df$bas.names[which(plot.df$bas.names=="Smr_CPUE_juv_ADFG_ln_scaled")] <- "Summer juvenile CPUE ADFG survey"
+plot.df$bas.names[which(plot.df$bas.names=="smr_adult_cond_scaled")] <- "Summer adult condition"
+
+
+
+
 # g <- ggplot(filter(plot.df, bas.names!='Intercept'),
 #             aes(x=bas.names, post.mean, fill=bas.names)) +
 #        theme_linedraw() +
@@ -439,8 +468,6 @@ g2
 # Bring Figs Together ========
 g3 <- plot_grid(g,g2, nrow=1, ncol=2, rel_widths=c(3,1), align='h')
 g3 #+ ggtitle('Sablefish Recruitment', subtitle=paste('Rsq:',round(,2)))
-ggsave(file=file.path(dir.figs,"BAS.png"), plot=g3, height=5, width=8, units='in',
-       dpi=500)
 
 
 #PLOT OUTPUT WITHOUT RAINBOW ===========
@@ -479,8 +506,7 @@ g2.b
 # Bring Figs Together ========
 g3.b <- plot_grid(g.b,g2.b, nrow=1, ncol=2, rel_widths=c(3,1), align='h')
 g3.b #+ ggtitle('Sablefish Recruitment', subtitle=paste('Rsq:',round(,2)))
-ggsave(file=file.path(dir.figs,"BAS_noRainbow.png"), plot=g3.b, height=5, width=8, units='in',
-       dpi=500)
+
 
 
 
