86bf15a8c12b8b8fb1dd4c101ffd5edd5ecb1c5b
KristaOke <kristaoke@gmail.com>
2023-06-14 10:24:34 -0800

    added a run with different algorthm and also fixed z.score=F instead of T since I'm using data that's already zscored

--- scripts/fit_DFA_sablefish.R
+++ scripts/fit_DFA_sablefish.R
@@ -178,7 +178,7 @@ for(R in levels.R) {
   for(m in 1:6) {  # allowing up to 3 trends
     dfa.model = list(A="zero", R=R, m=m)
     kemz = MARSS(z.mat1, model=dfa.model, control=cntl.list,
-                 form="dfa", z.score=TRUE)
+                 form="dfa", z.score=FALSE)
     model.data = rbind(model.data,
                        data.frame(R=R,
                                   m=m,
@@ -198,10 +198,55 @@ model.data1 <- model.data1 %>%
   arrange(dAICc)
 model.data1
 
-write_csv(model.data1, file = paste(wd,"/scripts/model_comparison_DFA_wholedataset.csv", sep=""))
+write_csv(model.data1, file = paste(wd,"/scripts/model_comparison_DFA_wholedataset_zscor-fixed.csv", sep=""))
 
 
 #---
+
+#try different algorithm see if convergence happens faster
+
+cntl.listB = list(minit=200, maxit=100000, allow.degen=FALSE, conv.test.slope.tol=0.1, abstol=0.0001)
+
+# set up forms of R matrices
+levels.R = c("diagonal and equal",
+             "diagonal and unequal",
+             "equalvarcov",
+             "unconstrained")
+
+#Run each and save output separately
+
+#training set 1 DFA-----
+model.data = data.frame()
+
+# fit models & store results
+for(R in levels.R) {
+  for(m in 1:6) {  # allowing up to 3 trends
+    dfa.model = list(A="zero", R=R, m=m)
+    kemz = MARSS(z.mat1, model=dfa.model, control=cntl.list,
+                 form="dfa", z.score=FALSE, method="BFGS")
+    model.data = rbind(model.data,
+                       data.frame(R=R,
+                                  m=m,
+                                  logLik=kemz$logLik,
+                                  K=kemz$num.params,
+                                  AICc=kemz$AICc,
+                                  stringsAsFactors=FALSE))
+    assign(paste("kemz", m, R, sep="."), kemz)
+  } # end m loop
+} # end R loop
+
+model.dataB <- model.data
+
+# calculate delta-AICc scores, sort in descending order, and compare
+model.dataB$dAICc <- model.dataB$AICc-min(model.dataB$AICc)
+model.dataB <- model.dataB %>%
+  arrange(dAICc)
+model.dataB
+
+write_csv(model.dataB, file = paste(wd,"/scripts/model_comparison_DFA_wholedataset_BFGS.csv", sep=""))
+
+
+
 
 #rotate=======
 
@@ -723,6 +768,129 @@ ccf(proc_rot[1, ], proc_rot[2, ], lag.max = 12, main = "")
 
 
 
+#plot 3 trend model=========
+
+
+# fitting first model with 3 trends (7th best)
+
+model.list.4 = list(A="zero", m=3, R="diagonal and unequal") # 
+model.4 = MARSS(z.mat1, model=model.list.4, z.score=TRUE, form="dfa", control=cntl.list4)
+#SEVERAL PARAMETERS DO NOT CONVERGE
+cntl.list4 = list(minit=200, maxit=300000, allow.degen=FALSE, conv.test.slope.tol=0.1, abstol=0.0001)
+
+autoplot(model.4)
+
+# and rotate the loadings
+Z.est = coef(model.4, type="matrix")$Z
+H_inv = varimax(coef(model.4, type="matrix")$Z)$rotmat
+Z.rot = as.data.frame(Z.est %*% H_inv)
+
+proc_rot = solve(H_inv) %*% model.4$states #doesn't work
+
+# reverse trend 2 to plot
+Z.rot[,2] <- -Z.rot[,2]
+
+Z.rot$names <- rownames(z.ind.mat)
+Z.rot <- arrange(Z.rot, V1)
+Z.rot <- gather(Z.rot[,c(1,2)])
+Z.rot$names <- rownames(z.ind.mat)
+#Z.rot$plot.names <- reorder(Z.rot$names, 1:14)
+
+
+
+# get CI and plot loadings...
+modCI <- MARSSparamCIs(model.4)
+
+plot.CI <- data.frame(mean=modCI$par$Z, upCI=modCI$par.upCI$Z,
+                      lowCI=modCI$par.lowCI$Z)
+
+plot.CI <- arrange(plot.CI, mean)
+#plot.CI$names.order <- reorder(plot.CI$names, plot.CI$mean)
+dodge <- position_dodge(width=0.9)
+
+rec.plot <- ggplot(Z.rot, aes(names, value, fill=key)) + geom_bar(stat="identity", position="dodge") #+
+# theme_bw() + ylab("Loading") + xlab("") + 
+# scale_fill_manual(values=c("Trend 1" = cb[2], "Trend 2" = cb[3])) +
+# theme(legend.position = c(0.8,0.2), legend.title=element_blank()) + geom_hline(yintercept = 0) +
+# theme(axis.text.x  = element_text(angle=45, hjust=1, size=12)) + ylim(-0.6, 0.8)
+
+#based on nwfsc-timeseries.github.io
+
+yr_frst <- 1977
+
+## get number of time series
+N_ts <- dim(z.ind.mat)[1]
+## get length of time series
+TT <- dim(z.ind.mat)[2]
+
+## get the estimated ZZ
+Z_est <- coef(model.4, type = "matrix")$Z
+## get the inverse of the rotation matrix
+H_inv <- varimax(Z_est)$rotmat
+
+## rotate factor loadings
+Z_rot = Z_est %*% H_inv
+## rotate processes
+proc_rot = solve(H_inv) %*% model.4$states
+
+mm <- 1 #processes
+
+rec_names <- rownames(z.ind.mat)
+ylbl <- rec_names
+w_ts <- seq(dim(z.ind.mat)[2])
+layout(matrix(c(1, 2), mm, 2), widths = c(2, 1))
+## par(mfcol=c(mm,2), mai=c(0.5,0.5,0.5,0.1), omi=c(0,0,0,0))
+# jpeg("figs/ugly_DFA_trends_loadings.jpg")
+par(mfcol=c(mm,2), mar = c(1,1,1,1), omi = c(0, 0, 0, 0))
+## plot the processes
+i<-1
+for (i in 1:mm) {
+  ylm <- c(-1, 1) * max(abs(proc_rot[i, ]))
+  ## set up plot area
+  plot(w_ts, proc_rot[i, ], type = "n", bty = "L", #ylim = ylm, 
+       xlab = "", ylab = "", xaxt = "n")
+  ## draw zero-line
+  abline(h = 0, col = "gray")
+  ## plot trend line
+  lines(w_ts, proc_rot[i, ], lwd = 2)
+  lines(w_ts, proc_rot[i, ], lwd = 2)
+  ## add panel labels
+  mtext(paste("State", i), side = 3, line = 0.5)
+  #axis(1, 12 * (0:dim(all.clim.dat)[2]) + 1, yr_frst + 0:dim(all.clim.dat)[2])
+  axis(1, 1:47, yr_frst + 0:dim(z.ind.mat)[2])
+}
+## plot the loadings
+clr <- c("brown", 
+         "blue", 
+         "darkgreen", 
+         "darkred", 
+         "purple", 
+         "darkorange")
+minZ <- 0
+ylm <- c(-1, 1) * max(abs(Z_rot))
+for (i in 1:mm) {
+  plot(c(1:N_ts)[abs(Z_rot[, i]) > minZ], as.vector(Z_rot[abs(Z_rot[, i]) > minZ, i]), 
+       type = "h", lwd = 2, xlab = "", ylab = "", 
+       xaxt = "n", ylim = ylm, xlim = c(0.5, N_ts + 0.5), col=clr)
+  for (j in 1:N_ts) {
+    if (Z_rot[j, i] > minZ) {
+      text(j, -0.03, ylbl[j], srt = 90, adj = 1, cex = 1.2, col=clr[j])
+    }
+    if (Z_rot[j, i] < -minZ) {
+      text(j, 0.03, ylbl[j], srt = 90, adj = 0, cex = 1.2, col=clr[j])
+    }
+    abline(h = 0, lwd = 1.5, col = "gray")
+  }
+  mtext(paste("Factor loadings on state", i), side = 3, line = 0.5)
+}
+#dev.off()
+
+par(mai = c(0.9, 0.9, 0.1, 0.1))
+ccf(proc_rot[1, ], proc_rot[2, ], lag.max = 12, main = "")
+
+
+
+
 #prep trends for regression-------
 #join trends data to data with recruitment
 
@@ -740,8 +908,22 @@ model2_reg_dat <- left_join(model.2_trends, scaled)
 
 quickmod2 <- lm(ln_rec ~ state1*state2, data=model2_reg_dat)
 anova(quickmod2)
-ggplot(model2_reg_dat, aes(state1, ln_rec)) + geom_point()+ geom_smooth()
-ggplot(model2_reg_dat, aes(state2, ln_rec)) + geom_point() + geom_smooth()
+ggplot(model2_reg_dat, aes(state1, ln_rec)) + geom_point()+ geom_smooth()+
+  geom_errorbar(aes(xmin=state1-state1_se, xmax=state1+state1_se))
+ggplot(model2_reg_dat, aes(state2, ln_rec)) + geom_point() + geom_smooth()+
+  geom_errorbar(aes(xmin=state2-state2_se, xmax=state2+state2_se))
+
+quickmod2.2 <- lm(ln_rec ~ state2, data=model2_reg_dat)
+anova(quickmod2.2)
+summary(quickmod2.2)
 
 #can I join to a dataset with non ln rec?
 ggplot(model2_reg_dat, aes(state2, Recruitment)) + geom_point() + geom_smooth()
+
+#compared to just cpue adfg?
+quickmod2_adfg <- lm(ln_rec ~ Smr_CPUE_juv_ADFG_ln_scaled, data=model2_reg_dat)
+anova(quickmod2_adfg)
+
+ggplot(model2_reg_dat, aes(Smr_CPUE_juv_ADFG_ln_scaled, ln_rec)) + geom_point() + geom_smooth()
+
+ggplot(model2_reg_dat, aes(sablefish_bycatch_arrowtooth_fishery_scaled, ln_rec)) + geom_point() + geom_smooth()
