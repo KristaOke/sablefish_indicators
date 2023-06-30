c0053d187e03cbfc8101a01ed1bb41c849ba8f27
KristaOke <kristaoke@gmail.com>
2023-06-30 13:12:11 -0800

    added some comments

--- scripts/fit_state-space_to_DFA_sablefish.R
+++ scripts/fit_state-space_to_DFA_sablefish.R
@@ -18,6 +18,21 @@ dir.figs <- file.path(wd,"figs")
 
 trends_rec_dat <- read.csv(file = paste(wd,"/data/DFA_trends_recruit_data.csv", sep=""))
 
+#this will load trends from two different DFAs
+
+#model 1 has a single trend onto which climate (mostly temperature) and juvenile
+#abundance metrics load
+
+#model 2 has two trends, temperature loads onto trend 1, juveniles load onto trend 2
+
+#ideally we would run separate state space models regressing the trends from
+#each model against ln_recruitment
+#e.g.
+# ln_rec ~ model1_state1 
+# AND
+# ln_rec ~ model2_state1 + model2_state2
+
+
 #=============================================================
 
 
