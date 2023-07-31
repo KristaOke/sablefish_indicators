db6060a8c8b9550ec619e00f5dcf0808d0b55220
KristaOke <kristaoke@gmail.com>
2023-07-31 15:04:16 -0800

    new script

---
+++ scripts/5yr_avg_recruitment.R
@@ -0,0 +1,36 @@
+#============================================================================================================================================
+# get 5-yr running avg recruitment to compare to models
+
+#Created by Krista, 2023
+#============================================================================================================================================
+#Notes:
+#============================================================================================================================================
+
+#=============================================================
+#### Define Directory Structure ####
+wd <- getwd()
+
+dir.data <- file.path(wd,"data")
+dir.output <- file.path(wd,"output")
+dir.figs <- file.path(wd,"figs")
+
+#Get data =======
+
+
+scaled <- read.csv(file=paste(wd,"/data/whole_dataset_scaled.csv", sep=""), row.names = 1)
+
+#THIS WILL VARY BY MODEL TYPE
+
+noncor_covars3 <- c("Year", "ln_rec",
+                    "Spr_ST_SEBS_scaled"  ,
+                    "Smr_temp_250m_GOA_scaled"  , 
+                    "Spr_chlA_biom_GOA_scaled"  , 
+                    "Spr_chlA_biom_SEBS_scaled"  ,  
+                    "Spr_chlA_peak_GOA_scaled" ,
+                    "Spr_chlA_peak_SEBS_scaled" ,
+                    "ann_Copepod_size_EGOA_scaled" ,
+                    "YOY_grwth_Middleton_scaled",
+                    "Smr_CPUE_juv_ADFG_ln_scaled"  ,      
+                    "smr_adult_cond_scaled")
+
+scaled_5yr_dat <- scaled[,names(scaled) %in% noncor_covars3]
